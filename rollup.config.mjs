8a95395a751de70bc3c826ee1490195607a33a6b
wyx.scottwu <wyx.scottwu@gmail.com>
2024-04-09 11:47:28 +0800

    💚 resolve rollup build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,25 @@
-import resolve from 'rollup-plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 export default {
-  input: './src/index.ts', // 入口文件
+  input: "./src/index.ts", // 入口文件
   output: [
     {
-      format: 'cjs', // 打包为commonjs格式
-      file: 'dist/index.cjs.js', // 打包后的文件路径名称
+      format: "cjs", // 打包为commonjs格式
+      file: "dist/index.cjs.js", // 打包后的文件路径名称
     },
     {
-      format: 'esm', // 打包为esm格式
-      file: 'dist/index.esm.js',
+      format: "esm", // 打包为esm格式
+      file: "dist/index.esm.js",
     },
     {
-      format: 'umd', // 打包为umd通用格式
-      file: 'dist/index.umd.js',
-      minifyInternalExports: true
-    }
+      format: "umd", // 打包为umd通用格式
+      file: "dist/index.umd.js",
+      name: "lw-kit",
+      minifyInternalExports: true,
+    },
   ],
-  plugins: [typescript({ tsconfig: './tsconfig.json' }), resolve()]
-}
+  context: "window",
+  external: ["lodash"], // 将 lodash 声明为外部依赖
+  plugins: [typescript({ tsconfig: "./tsconfig.json" }), resolve(), commonjs()],
+};
